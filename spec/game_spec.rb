fe1fb914c60b0618b819de4601852343a767cfb0
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-07 17:11:52 +0000

    Refactor to pass players into game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,22 @@
 require 'game'
 describe Game do
 
-  let(:player){ double :player_1 }
+  let(:player_1){ double :player_1 }
+  let(:player_2) { double :player_2 }
+  subject { described_class.new(player_1, player_2)}
+  
+
+  it 'can accept two players' do
+    expect(described_class).to respond_to(:new).with(2).arguments
+  end
+
+  it 'returns player 1' do
+    expect(subject.player_1).to eq player_1
+  end
+
+  it 'returns player 2' do
+    expect(subject.player_2).to eq player_2
+  end
 
   describe '#attack' do
     it "accepts one argument" do
@@ -9,9 +24,9 @@ describe Game do
     end
     
     it 'damages the player' do
-      allow(player).to receive(:receive_damage)
-      expect(player).to receive(:receive_damage)
-      subject.attack(player)
+      allow(player_1).to receive(:receive_damage)
+      expect(player_1).to receive(:receive_damage)
+      subject.attack(player_1)
     end
   end
 end
